e01c98a06b903f106829b0b0091f074a6e89743a
h-j-k <han.junkwang@gmail.com>
2014-09-25 00:59:51 +0800

    added class javadoc and other minor changes.

--- src/main/java/com/ikueb/collatz/CollatzUtils.java
+++ src/main/java/com/ikueb/collatz/CollatzUtils.java
@@ -5,6 +5,13 @@ import java.util.stream.LongStream;
 
 /**
  * Utilities class for deriving number of steps using the Collatz conjecture.
+ * <p>
+ * Given a number <code>n</code>:
+ * <ul>
+ * <li>If <code>n</code> is even, repeat for value <code>n / 2</code>.</li>
+ * <li>If <code>n</code> is odd, repeat for value <code>3 * n + 1</code>.</li>
+ * <li>Repeat until <code>n = 1</code>.</li>
+ * </ul>
  */
 public final class CollatzUtils {
 
@@ -33,7 +40,7 @@ public final class CollatzUtils {
             throw new IllegalArgumentException("value must be greater than 0.");
         }
         final AtomicLong c = new AtomicLong();
-        LongStream.iterate(value, (v) -> c.incrementAndGet() > 0 && v % 2 == 0 ? v / 2 : 1 + 3 * v)
+        LongStream.iterate(value, (v) -> c.incrementAndGet() > 0 && v % 2 == 0 ? v / 2 : 3 * v + 1)
                 .anyMatch(v -> c.longValue() > 1 && v == 1);
         return c.decrementAndGet();
     }
--- src/test/java/com/ikueb/collatz/CollatzUtilsTest.java
+++ src/test/java/com/ikueb/collatz/CollatzUtilsTest.java
@@ -3,9 +3,9 @@ package com.ikueb.collatz;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.stream.LongStream;
-import java.util.stream.Stream;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -26,30 +26,30 @@ public class CollatzUtilsTest {
 
     @DataProvider(name = "test-cases")
     public Iterator<Object[]> getTestCases() {
-        return Stream.of(TestCase.values())
+        return Arrays.stream(TestCase.values())
                 .map((current) -> new Object[] { current.value, current.expected }).iterator();
     }
 
     @DataProvider(name = "twos-test-cases")
     public Iterator<Object[]> getTwosTestCases() {
-        return LongStream.range(1, 11).boxed()
+        return LongStream.range(1, 61).boxed()
                 .map((v) -> new Object[] { Long.valueOf((long) Math.pow(2, v.doubleValue())), v })
                 .iterator();
     }
 
     @Test(dataProvider = "test-cases")
-    public void testFirstTen(Long value, Long expected) {
+    public void testCaseFor(Long value, Long expected) {
         assertThat(Long.valueOf(CollatzUtils.stepsFor(value.longValue())), equalTo(expected));
     }
 
     @Test(dataProvider = "twos-test-cases")
     public void testPowersOfTwo(Long value, Long expected) {
-        assertThat(Long.valueOf(CollatzUtils.stepsFor(value.longValue())), equalTo(expected));
+        testCaseFor(value, expected);
     }
 
     @Test
     public void testLargeValue() {
-        assertThat(Long.valueOf(CollatzUtils.stepsFor(9780657631L)), equalTo(Long.valueOf(1132)));
+        testCaseFor(Long.valueOf(9780657631L), Long.valueOf(1132));
     }
 
     @Test(expectedExceptions = IllegalArgumentException.class)
