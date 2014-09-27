4cf92e3fdf7d132242158ab59042fd4cef2d4620
h-j-k <han.junkwang@gmail.com>
2014-09-27 10:21:15 +0800

    replaced AtomicLong with long[1] and improved Javadoc.

--- src/main/java/com/ikueb/collatz/CollatzUtils.java
+++ src/main/java/com/ikueb/collatz/CollatzUtils.java
@@ -1,7 +1,7 @@
 package com.ikueb.collatz;
 
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 /**
  * Utilities class for deriving number of steps using the Collatz conjecture.
@@ -30,6 +30,10 @@ public final class CollatzUtils {
      * <li>1: 1 &#8594; 4 &#8594; 2 &#8594; 1. Therefore, the return value is 3.</li>
      * <li>2: 2 &#8594; 1. Therefore, the return value is 1.</li>
      * </ul>
+     * <p>
+     * This implementation works by using a {@link Stream#anyMatch(java.util.function.Predicate)}
+     * short-circuiting terminal operation, so although the right answer is returned at the end, it
+     * does alter the definition of a stream of Collatz sequence numbers.
      *
      * @param value the value to start from.
      * @return the number of steps to end at 1.
@@ -39,10 +43,10 @@ public final class CollatzUtils {
         if (value < 1) {
             throw new IllegalArgumentException("Input must be greater than 0.");
         }
-        final AtomicLong c = new AtomicLong();
-        LongStream.iterate(value, (v) -> c.incrementAndGet() > 0 && v % 2 == 0 ? v / 2 : 3 * v + 1)
-                .anyMatch(v -> c.longValue() > 1 && v == 1);
-        return c.decrementAndGet();
+        final long[] counter = new long[1];
+        LongStream.iterate(value, (v) -> ++counter[0] > 0 && v % 2 == 0 ? v / 2 : 3 * v + 1)
+                .anyMatch(v -> counter[0] > 1 && v == 1);
+        return --counter[0];
     }
 
 }
